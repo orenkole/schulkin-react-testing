9f76f538ca8a715b666f1336dde5df2d41b950f3
badger <badger@badger.local>
2022-06-17 20:37:14 +0300

    Click button to change color

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
+import {useState} from "react";
 
 function App() {
-  return (
-    <div>
-      <button style={{backgroundColor: 'red'}}>Change to blue</button>
-    </div>
-  );
+    const [buttonColor, setButtonColor] = useState('red')
+    const newButtonColor = buttonColor === 'red' ? 'blue' : 'red';
+    return (
+        <div>
+            <button
+                style={{backgroundColor: buttonColor}}
+                onClick={() => {setButtonColor(newButtonColor)}}
+            >
+                Change to {newButtonColor}
+            </button>
+        </div>
+    );
 }
 
 export default App;
--- src/App.test.js
+++ src/App.test.js
@@ -1,14 +1,13 @@
-import { render, screen } from '@testing-library/react';
+import {fireEvent, render, screen} from '@testing-library/react';
 import App from './App';
 
 test("button has correct initial color", () => {
-  debugger;
-  console.log(App);
   render(<App />);
   const colorButton = screen.getByRole('button', {name: 'Change to blue'});
   expect(colorButton).toHaveStyle({backgroundColor: 'red'})
-})
 
-test("button turns blue when clicked", () => {
-
+  // click button
+  fireEvent.click(colorButton);
+  expect(colorButton).toHaveStyle({backgroundColor: 'blue'})
+  expect(colorButton.textContent).toBe('Change to red')
 })
