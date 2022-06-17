669ee57a7cd92b62c7e68d4820e18c6595334d5f
badger <badger@badger.local>
2022-06-17 20:23:26 +0300

    Start color button app

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,14 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test("button has correct initial color", () => {
+  debugger;
+  console.log(App);
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+  const colorButton = screen.getByRole('button', {name: 'Change to blue'});
+  expect(colorButton).toHaveStyle({backgroundColor: 'red'})
+})
+
+test("button turns blue when clicked", () => {
+
+})
