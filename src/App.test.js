fffea51df5715c450b5ee6fa42aa64f9a80fe035
badger <badger@badger.local>
2022-06-17 20:49:37 +0300

    Test Initial Condition of Button and Checkbox

--- src/App.test.js
+++ src/App.test.js
@@ -11,3 +11,13 @@ test("button has correct initial color", () => {
   expect(colorButton).toHaveStyle({backgroundColor: 'blue'})
   expect(colorButton.textContent).toBe('Change to red')
 })
+
+test('Initial conditions', () => {
+  render(<App/>)
+  // check that button starts out enabled
+  const colorButton = screen.getByRole('button', {name: 'Change to blue'})
+  expect(colorButton).toBeEnabled();
+  // check that the checkbox starts out unchecked
+  const checkbox = screen.getByRole('checkbox');
+  expect(checkbox).not.toBeChecked();
+})
